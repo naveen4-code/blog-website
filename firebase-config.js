63941b0ed881277734b95b45ab94e9981237e823
Naveen Raj K <knaveenraj725@gmail.com>
2025-12-06 10:36:37 +0530

    Update firebase-config.js
    Updated Firebase configuration with new credentials.

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABr2d7ZtS590dmHETwAWHEF5ugQvnI6n0",
-  authDomain: "blog-website-1c1aa.firebaseapp.com",
+  apiKey: "Axxxxxxx",
+  authDomain: "xxxxxxx",
   projectId: "blog-website-1c1aa",
-  storageBucket: "blog-website-1c1aa.firebasestorage.app",
-  messagingSenderId: "613804563206",
-  appId: "1:613804563206:web:4932e50405bfbdf3f3dc87"
+  storageBucket: "xxxxxxx",
+  messagingSenderId: "6xxxxxx",
+  appId: "xxxxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
